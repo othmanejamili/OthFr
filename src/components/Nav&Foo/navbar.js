afe9493656d0ef87fd721ad75d3adab05c0cb898
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2025-07-17 17:11:38 +0100

    correct

--- src/components/Nav&Foo/navbar.js
+++ src/components/Nav&Foo/navbar.js
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from "react";
 import { useLocation } from "react-router-dom";
 import gsap from "gsap";
 import '../../styles/Navbar.css';
+import { ShoppingBag } from 'lucide-react';
 
 const NavBar = () => {
   const navbarRef = useRef(null);
@@ -121,9 +122,8 @@ const NavBar = () => {
       { href: "/login", label: "Login" },
       { 
         href: "/cart", 
-        label:<svg width="30" height="30" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M3 3H5L5.4 5M7 13H17L21 5H5.4M7 13L5.4 5M7 13L4.7 15.3C4.3 15.7 4.6 16.5 5.1 16.5H17M17 13V16.5M9 19.5C9.8 19.5 10.5 20.2 10.5 21S9.8 22.5 9 22.5 7.5 21.8 7.5 21 8.2 19.5 9 19.5ZM20 19.5C20.8 19.5 21.5 20.2 21.5 21S20.8 22.5 20 22.5 18.5 21.8 18.5 21 19.2 19.5 20 19.5Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-        </svg>
+        label:  <ShoppingBag />
+
       }
     ];
 
