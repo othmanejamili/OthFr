a339cf235af601ce7a2a39595613e72954cb5fe3
MacBook Pro <macbookpro@192.168.100.200>
2025-09-09 18:16:13 +0100

    fix the bug comment

--- src/components/Product/ProductComment.js
+++ src/components/Product/ProductComment.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
 import { Star, MessageCircle, User } from 'lucide-react';
 import { AuthContext } from '../../context/AuthContext';
+import '../../styles/comment.css';
 
 const ProductComments = ({ productId }) => {
   const { currentUser, isAuthenticated, getToken } = useContext(AuthContext);
