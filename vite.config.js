b6d3721804a4d044f85b3a59be9fc2289016ce9a
Mac Pro <mac@192.168.100.119>
2025-09-16 19:23:03 +0100

    Fix Vercel config and remove conflicting scripts

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    include: /\.(jsx?|tsx?)$/,
-    loader: 'jsx',
-  },
   build: {
     outDir: 'dist'
   }
